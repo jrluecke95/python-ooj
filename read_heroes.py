2021698a3d40c898c93e2441d9e368bc25271324
Jake Luecke <jrluecke95@gmail.com>
2021-01-14 15:45:49 -0500

    updated pickle demo

--- read_heroes.py
+++ read_heroes.py
@@ -1,6 +1,12 @@
 import pickle
 
-message = "marvel is better than Dc"
+with open('heroes.txt', 'rb') as file_handle:
+    old_message = pickle.load(file_handle)
+    print(f"Old message is: '{old_message}'")
+
+yser_input = input("What to save? ")
 
 with open('heroes.txt', 'wb') as file_handle:
     pickle.dump(message, file_handle)
+    print("message saved. ")
+
