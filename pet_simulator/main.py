a3ad59a933ae9f3d1be6f58c3ee6464a8e5bd4ec
Jake Luecke <jrluecke95@gmail.com>
2021-01-19 16:55:31 -0500

    lots o changes

--- pet_simulator/main.py
+++ pet_simulator/main.py
@@ -1,4 +1,7 @@
 from pet import Pet, CuddlyPet
+from toy import Toy
+from treat import ColdPizza, Bacon, VeganSnack
+from menu import Menu
 
 # Begin with no pets.
 pets = []
@@ -8,34 +11,63 @@ main_menu = [
     "Play with Pet",
     "Feed Pet",
     "View status of pets",
+    "Give a toy to all your pets",
     "Do nothing",
+    "Give your pets a snack"
 ]
 
-def print_menu_error():
-    print("That was not a valid choice. Try again.\n\n\n")    
+adoption_menu = [
+    "Pet",
+    "Cuddly Pet"
+]
 
-def choices_to_string(choice_list):
-    choice_string = ""
-    num = 1
-    for choice in choice_list:
-        choice_string += "%d: %s\n" % (num, choice)
-        num += 1
-    choice_string += "Please choose an option: "
-    return choice_string
+treat_menu = [
+    "ColdPizza",
+    "Bacon",
+    "VeganSnack"
+]
 
-def get_user_choice(choice_list):
-    choice = -1
-    choice_string = choices_to_string(choice_list)
-    while choice == -1:
-        try:
-            choice = int(input(choice_string))
-            if choice <= 0 or choice > len(choice_list):
-                raise ValueError
-        except ValueError:
-            print_menu_error()
-    return choice
-
-def main():    
+def main():
+    app = Menu("Please choose an option", main_menu)
+    types = Menu("Please choose a type of pet", adoption_menu)
+    treats = Menu("please choose a type of treat", treat_menu)
     while True:
-        choice = get_user_choice(main_menu)
+        choice = app.get_user_choice()
+        if choice == 1:
+            pet_name = input("What would you like the pet name to be?")
+            type_choice = types.get_user_choice()
+            if type_choice == 1:
+                pets.append(Pet(pet_name))
+            elif type_choice == 2:
+                pets.append(CuddlyPet(pet_name))
+            num_pets = len(pets)
+            print(f"You now have {num_pets} pets")
+        elif choice == 2:
+            for pet in pets:
+                pet.get_love()
+        elif choice == 3:
+            for pet in pets:
+                pet.eat_food()
+        elif choice == 4:
+            for pet in pets:
+                print(pet)
+        elif choice == 5:
+            for pet in pets:
+                pet.get_toy(Toy())
+        elif choice == 6:
+            for pet in pets:
+                pet.be_alive()
+        elif choice == 7:
+            print("Please choose your type of treat:")
+            treat_choice = treats.get_user_choice
+            if treat_choice == 1:
+                for pet in pets:
+                    pet.eat_treat(ColdPizza())
+            elif treat_choice == 2:
+                for pet in pets:
+                    pet.eat_treat(Bacon())
+            elif treat_choice == 3:
+                for pet in pets:
+                    pet.eat_treat(VeganSnack())
+
 main()
--- pet_simulator/pet.py
+++ pet_simulator/pet.py
@@ -5,6 +5,7 @@ class Pet:
         self.happiness = happiness
         self.hunger = hunger
         self.mopiness = mopiness
+        self.toys = []
     
     def eat_food(self):
         self.fullness += 30
@@ -15,16 +16,41 @@ class Pet:
     def be_alive(self):
         self.fullness -= self.hunger
         self.happiness -= self.mopiness
+        for toy in self.toys:
+            self.happiness += toy.use()
+    
+    def __str__(self):
+        return f"""
+        {self.name}:
+            Fullness: {self.fullness}
+            Happiness: {self.happiness}
+        """
+    
+    def eat_treat(self, treat):
+        self.fullness += treat.yum
+        self.happiness += treat.joy
+    
+    def get_toy(self, toy):
+        self.toys.append(toy)
+
 
 class CuddlyPet(Pet):
     def __init__(self, name, fullness = 50, hunger = 5):
         super().__init__(name, fullness, 100, hunger, 2)
 
+    def __str__(self):
+        return f"""
+        Extra Cuddly:
+        Fullness: {self.fullness}
+        Happiness: {self.happiness}
+        """
 
     def cuddle(self, other_pet):
         other_pet.get_love()
     
     def be_alive(self):
-        self.fullness -= self.hunger
+        super().be_alive()
         self.happiness -= self.mopiness/2
-    
+
+
+
